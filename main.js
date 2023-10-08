3b775dbf5d62a91fc72c91b9efc0a977b15e50ba
Laisa Marques <laisamarkss@gmail.com>
2023-10-08 14:50:43 -0300

    agenda_contatos

---
+++ main.js
@@ -0,0 +1,7 @@
+function label () {
+    var Nome = document.getElementById('Nome').value;
+    var Telefone = document.getElementById('Telefone').value;
+    var Cadastrar = document.getElementById('Cadastrar').value;
+
+    
+}
