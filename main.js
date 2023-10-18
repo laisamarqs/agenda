a4ad538fa3695707cc06aa25d4d18796c5bd5e80
Laisa Marques <laisamarkss@gmail.com>
2023-10-18 15:16:37 -0300

    agenda

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-function label () {
-    var Nome = document.getElementById('Nome').value;
-    var Telefone = document.getElementById('Telefone').value;
-    var Cadastrar = document.getElementById('Cadastrar').value;
+agenda/index.html
 
-    
-}
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.getElementById('form-agenda');
+        const table = document.querySelector('table');
+        
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            
+            const nome = document.getElementById('Nome').value;
+            const telefone = document.getElementById('Telefone').value;
+
+            
+            const newRow = table.insertRow(table.rows.length);
+            const cell1 = newRow.insertCell(0);
+            const cell2 = newRow.insertCell(1);
+
+            
+            cell1.innerHTML = nome;
+            cell2.innerHTML = telefone;
+
+            
+            form.reset();
+        });
+    });
+
